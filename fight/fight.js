c8e2d2efc63777201d92d0e721b564fc5c6077ae
Gerben <99052865@mydavinci.nl>
2020-02-14 13:38:46 +0100

    Enemy attack
    the enemy now attacks you

--- fight/fight.js
+++ fight/fight.js
@@ -3,31 +3,38 @@ var elvl = 10;//enemy's level (just for show)
 var maxEHP = 300;//enemy's max health
 var EHP = 200; //enemy's current health
 
-var name = "pikachu";//this lets the player choose a custom name in the settings.
-var lvl = 10;//players level (just for show)
-var maxHP = 300;//players max health
-var HP = 200; //players current health
+var pokemon = [
+["pikachu", 10, 300, 200], 
+["charmander", 10, 300, 200] , 
+["totodile", 10, 300, 300], 
+["geodude", 10, 300, 300]
+];
+
+var name = pokemon[0][0];//this lets the player choose a custom name in the settings.
+var lvl = pokemon[0][1];//players level (just for show)
+var maxHP = pokemon[0][2];//players max health
+var HP = pokemon[0][3]; //players current health
 
 var bagOpen = false; //the state of the menu's
-var yourTurn = true;
 
-var pokemon = ["pikachu", "charmander" , "totodile", "geodude"]; 
-
-var currentPokemon = pokemon[0];
+var currentPokemon = [pokemon[0][0], 0];
 
 var item = ["potion", 20, "super potion", 50, "hyper potion", 200, "max potion", maxHP];
 
 //var atck1 = [name, dmg, effect, life gian, max pp, current pp (how many times an attack can be used)]
+var attacks = {
+	atck1: ["tackle", 10, 0, 0, 10, 10],
+	atck2: ["tackle", 10, 0, 0, 10, 10],
+	atck3: ["tackle", 10, 0, 0, 10, 10],
+	atck4: ["tackle", 10, 0, 0, 10, 10]
+}
 
-var atck1 = ["tackle", 10, 0, 0, 10, 10]; //player attack info
-var atck2 = ["tackle", 10, 0, 0, 10, 10];
-var atck3 = ["tackle", 10, 0, 0, 10, 10];
-var atck4 = ["tackle", 10, 0, 0, 10, 10];
-
-var eatck1 = ["tackle", 10, 0, 0, 10, 10]; //enemy attack info
-var eatck2 = ["tackle", 10, 0, 0, 10, 10]; 
-var eatck3 = ["tackle", 10, 0, 0, 10, 10]; 
-var eatck4 = ["tackle", 10, 0, 0, 10, 10];
+var eattacks = {
+	atck1: ["tackle", 10, 0, 0, 10, 10],
+	atck2: ["tackle", 10, 0, 0, 10, 10],
+	atck3: ["tackle", 10, 0, 0, 10, 10],
+	atck4: ["tackle", 10, 0, 0, 10, 10]
+}
 
 var button1 = document.getElementById("op1");
 var button2 = document.getElementById("op2");
@@ -46,20 +53,19 @@ document.getElementById("hpBar").style.width = HP/maxHP*document.getElementById(
 document.getElementById("text").innerHTML = "What will " + name + " do?";
 
 function battleButtons() {
-
 	bagOpen = true;
 
-	button1.innerHTML = atck1[0];
-	button1.setAttribute("onclick" , "options(atck1)");
+	button1.innerHTML = attacks.atck1[0];
+	button1.setAttribute("onclick" , "options(attacks.atck1)");
 	
-	button2.innerHTML = atck2[0];
-	button2.setAttribute("onclick" , "options(atck2)");
+	button2.innerHTML = attacks.atck2[0];
+	button2.setAttribute("onclick" , "options(attacks.atck2)");
 	
-	button3.innerHTML = atck3[0];
-	button3.setAttribute("onclick" , "options(atck3)");
+	button3.innerHTML = attacks.atck3[0];
+	button3.setAttribute("onclick" , "options(attacks.atck3)");
 
-	button4.innerHTML = atck4[0];
-	button4.setAttribute("onclick" , "options(atck4)");
+	button4.innerHTML = attacks.atck4[0];
+	button4.setAttribute("onclick" , "options(attacks.atck4)");
 
 	document.getElementById("text").innerHTML = "What attack will " + name + " use?"
 }//makes you look at your attacks
@@ -71,26 +77,32 @@ if (bagOpen == true &&  e.keyCode == "13") {
 });//if the enter key is pressed while the player is in a menu they get sent back to the options
 
 function options() {
-	yourTurn = false;
-	optionButtons()
-	atck1[5] = atck1[5] - 1;
+	if (arguments[0][5] > 0) {//max uses
+		arguments[0][5] = arguments[0][5] - 1;
 
-	if (EHP - atck1[1] >= 0) {
-		EHP = EHP - atck1[1];
-	}else{
-		EHP = 0;
-	}
-	
-	if ((HP + atck1[3]) <= maxHP) {
-		HP = HP + atck1[3];
+		if (EHP - arguments[0][1] > 0) {
+			EHP = EHP - arguments[0][1];
+		}else{
+			EHP = 0;
+			document.getElementById("text").innerHTML = "" + ename + " has fainted!";
+		}
+		
+		if ((HP + arguments[0][3]) <= maxHP) {
+			HP = HP + arguments[0][3];
+		}else {
+			HP = maxHP;
+		}
+
+		document.getElementById("ehpBar").style.width = EHP/maxEHP*document.getElementById("ehpBarGrey").clientWidth + "px"; //adjusts the hpbar
+		document.getElementById("hpBar").style.width = HP/maxHP*document.getElementById("hpBarGrey").clientWidth + "px"; //adjusts the hpbar
+		document.getElementById("hp").innerHTML = HP + " / " + maxHP;
+		document.getElementById("text").innerHTML = "" + name + " used " + arguments[0][0] + "";
+		pokemon[currentPokemon[1]][3] = HP;
+		enemyTurn()
 	}else {
-		HP = maxHP;
+		document.getElementById("text").innerHTML = "You dont have any more uses left for this attack.";
 	}
 
-	document.getElementById("ehpBar").style.width = EHP/maxEHP*document.getElementById("ehpBarGrey").clientWidth + "px"; //adjusts the hpbar
-	document.getElementById("hpBar").style.width = HP/maxHP*document.getElementById("hpBarGrey").clientWidth + "px"; //adjusts the hpbar
-	document.getElementById("hp").innerHTML = HP + " / " + maxHP;
-	//make max uses
 }//Uses your attack
 
 function bagButtons() {
@@ -120,30 +132,38 @@ function use() {
 
 	document.getElementById("hpBar").style.width = HP/maxHP*document.getElementById("hpBarGrey").clientWidth + "px"; //adjusts the hpbar
 	document.getElementById("hp").innerHTML = HP + " / " + maxHP;
+	pokemon[currentPokemon[1]][3] = HP;
 
 	optionButtons();
 }
 
 function pokemonButtons() {
 	bagOpen = true;
-	button1.innerHTML = pokemon[0];
+	button1.innerHTML = pokemon[0][0];
 	button1.setAttribute("onclick" , "changePokemon(0)");
 	
-	button2.innerHTML = pokemon[1];
+	button2.innerHTML = pokemon[1][0];
 	button2.setAttribute("onclick" , "changePokemon(1)");
 	
-	button3.innerHTML = pokemon[2];
+	button3.innerHTML = pokemon[2][0];
 	button3.setAttribute("onclick" , "changePokemon(2)");
 
-	button4.innerHTML = pokemon[3];
+	button4.innerHTML = pokemon[3][0];
 	button4.setAttribute("onclick" , "changePokemon(3)");
 
 	document.getElementById("text").innerHTML = "What pokemon will you use?"
 }//need to make an array to contain pokemon data
 
 function changePokemon() {
-	currentPokemon = pokemon[arguments[0]];
-	name = pokemon[arguments[0]];
+	currentPokemon[0] = pokemon[arguments[0]];
+	currentPokemon[1] = arguments[0];
+	name = pokemon[arguments[0]][0];
+	lvl = pokemon[arguments[0]][1];
+	maxHP = pokemon[arguments[0]][2];
+	HP = pokemon[arguments[0]][3];
+
+	document.getElementById("hpBar").style.width = HP/maxHP*document.getElementById("hpBarGrey").clientWidth + "px"; //adjusts the hpbar
+	document.getElementById("hp").innerHTML = HP + " / " + maxHP;
 	document.getElementById("name").innerHTML = name;
 
 	optionButtons();
@@ -169,3 +189,24 @@ function optionButtons() {
 function titleScreen() {
 	window.location.href = "../titleScreen/titleScreen.html";
 }//brings you back to the title screen
+
+function enemyTurn() {
+	var whatAttack = eattacks["atck" + Math.floor((Math.random() * 4) + 1)];
+
+		if (HP - whatAttack[1] > 0) {
+			HP = HP - whatAttack[1];
+		}else{
+			HP = 0;
+			document.getElementById("text").innerHTML = "" + name + " has fainted!";
+		}
+		
+		if ((EHP + whatAttack[3]) <= maxEHP) {
+			EHP = EHP + whatAttack[3];
+		}else {
+			EHP = maxEHP;
+		}
+
+		
+	
+	setTimeout(optionButtons(), 4000);
+}
